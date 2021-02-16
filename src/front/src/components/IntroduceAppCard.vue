1fa65786e2970976259c390c99d1b5e97e195647
shinjiezumi <shinjiezumi.work@gmail.com>
2021-02-16 20:03:36 +0900

    fixキャプチャ

--- src/front/src/components/IntroduceAppCard.vue
+++ src/front/src/components/IntroduceAppCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="mx-auto" elevation="5">
-    <v-img :src="appImage" class="appImage"/>
+    <v-img :src="appImage" height="400px" />
     <v-card-title>{{ appTitle }}</v-card-title>
     <v-card-subtitle class="appSubTitle">{{ appSubTitle }}</v-card-subtitle>
     <v-card-actions>
@@ -11,6 +11,7 @@
 
 <script>
 export default {
+  name: "IntroduceAppCard",
   props: {
     "appTitle": {
       type: String,
@@ -29,10 +30,6 @@ export default {
 </script>
 
 <style scoped>
-.appImage {
-  height: 250px;
-}
-
 .appSubTitle {
   height: 2rem;
   max-height: 2rem;
